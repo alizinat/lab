84e23fec0065f59bdae754af088235f67daf068e
alizinat <103899932+alizinat@users.noreply.github.com>
2024-03-10 04:51:27 +0000

    Create qualys_win_install.sh

---
+++ qualys_win_install.sh
@@ -0,0 +1,1 @@
+QualysCloudAgent.exe CustomerId={20dfcf16-d261-deac-8020-3b9208204b45} ActivationId={9320ff00-cf8b-4874-acaf-bf153bc72311} WebServiceUri=https://qagpublic.qg1.apps.qualys.co.uk/CloudAgent/
