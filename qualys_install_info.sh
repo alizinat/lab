53d826f2cb5c5c7ef5b5bafe23a348fdc83d4de9
alizinat <103899932+alizinat@users.noreply.github.com>
2024-03-10 03:45:49 +0000

    Create qualys_install_info.sh

---
+++ qualys_install_info.sh
@@ -0,0 +1,2 @@
+sudo dpkg --install QualysCloudAgent.deb 
+sudo /usr/local/qualys/cloud-agent/bin/qualys-cloud-agent.sh ActivationId=bd1035c9-660c-4cbd-8744-395bca107f0b CustomerId=20dfcf16-d261-deac-8020-3b9208204b45 ServerUri=https://qagpublic.qg1.apps.qualys.co.uk/CloudAgent/
